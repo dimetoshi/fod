981c51b5f054210f56ecfdd7ef0e76e732871166
dimetoshi <design@clrk.studio>
2025-01-02 19:20:09 +1030

    Update ProfileGrid.js

--- components/ProfileGrid.js
+++ components/ProfileGrid.js
@@ -239,7 +239,7 @@ const ProfileGrid = () => {
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
         {profiles.map((profile, index) => (
           <ProfileCard key={index} {...profile} />
         ))}
