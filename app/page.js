1dff20c573f9331b943b2da59c67bac4f5174696
dimetoshi <design@clrk.studio>
2025-06-04 07:49:12 +0930

    Update page.js

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
 
   return (
     <main>
-      <DogeGovNavComponent options={navOptions} />
+      {/* <DogeGovNavComponent options={navOptions} /> */}
       <ProfileGrid />
     </main>
   )
