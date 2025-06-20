84e0f4aed59eeca85d06cd23924b7950393ff13e
dimetoshi <design@clrk.studio>
2025-06-20 17:03:29 +0930

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,13 +5,39 @@ const inter = Inter({ subsets: ['latin'] })
 const oswald = Oswald({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Profile Grid',
-  description: 'Profile Grid Example',
+  title: 'Friends of DOGE | DogeGov Memecoin',
+  description: 'Meet the key figures supporting the Department of Government Efficiency (DOGE). From Elon Musk and Vivek Ramaswamy to politicians and entrepreneurs working to streamline government operations.',
+  openGraph: {
+    title: 'Friends of DOGE | DogeGov Memecoin',
+    description: 'Meet the key figures supporting the Department of Government Efficiency (DOGE). From Elon Musk and Vivek Ramaswamy to politicians and entrepreneurs working to streamline government operations.',
+    url: 'https://dogegov.com/friendsofdoge.html',
+    siteName: 'DogeGov',
+    images: [
+      {
+        url: 'https://doge-memes.b-cdn.net/doge-thumb-1200x620.jpg',
+        width: 1200,
+        height: 620,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Friends of DOGE | DogeGov Memecoin',
+    description: 'Meet the key figures supporting the Department of Government Efficiency (DOGE). From Elon Musk and Vivek Ramaswamy to politicians and entrepreneurs working to streamline government operations.',
+    images: ['https://doge-memes.b-cdn.net/doge-thumb-1200x620.jpg'],
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon/favicon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      </head>
       <body className={`${inter.className} ${oswald.className} bg-black m-0 p-0`}>
         {children}
       </body>
