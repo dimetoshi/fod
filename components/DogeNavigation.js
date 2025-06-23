818d6ebe196ec5b1491d414ea564273ce9cff7c4
dimetoshi <design@clrk.studio>
2025-06-24 08:11:02 +0930

    Update DogeNavigation.js

--- components/DogeNavigation.js
+++ components/DogeNavigation.js
@@ -83,10 +83,10 @@ const DogeNavigation = () => {
 
   const navLinks = [
     { text: 'Memes', href: 'https://t.me/+eB3Zaw8uKj84YjNh', target: '_blank' },
-    { text: 'Buy/Trade', href: 'https://dogegov.com/buy-trade.html' },
-    { text: 'Doge Clock', href: 'https://dogegov.com/dogeclock.html' },
+    { text: 'Buy/Trade', href: 'https://dogegov.com/buy.html' },
+    { text: 'Doge Clock', href: 'https://dogegov.com/doge-clock.html' },
     { text: 'Doge Compare', href: 'https://dogegov.com/dogecompare.html' },
-    { text: 'Friends of DOGE', href: 'https://dogegov.com/friendsofdoge.html' },
+    { text: 'Friends of DOGE', href: 'https://dogegov.com/friends-of-doge.html' },
     { text: 'Agent ID', href: 'https://agent.dogegov.com/', target: '_blank' },
     { text: 'Merch', href: 'https://shop.dogegov.com/', target: '_blank' },
     { text: 'BUY $DOGE', href: 'https://shop.dogegov.com/', target: '_blank', primary: true }
@@ -94,13 +94,13 @@ const DogeNavigation = () => {
 
   const socialLinks = [
     { 
-      name: 'Twitter',
-      href: 'https://twitter.com/dogegov',
+      name: 'X',
+      href: 'https://x.com/doge_eth_gov',
       svg: 'M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z'
     },
     {
       name: 'Telegram',
-      href: 'https://t.me/dogegov',
+      href: 'https://t.me/NewDOGEonETH_CTO',
       svg: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19-.14.75-.42 1-.68 1.03-.58.05-1.02-.38-1.58-.75-.88-.58-1.38-.94-2.23-1.5-.99-.65-.35-1.01.22-1.59.15-.15 2.71-2.48 2.76-2.69a.2.2 0 00-.05-.18c-.06-.05-.14-.03-.21-.02-.09.02-1.49.95-4.22 2.79-.4.27-.76.41-1.08.4-.36-.01-1.04-.2-1.55-.37-.63-.2-1.12-.31-1.08-.66.02-.18.27-.36.74-.55 2.92-1.27 4.86-2.11 5.83-2.51 2.78-1.16 3.35-1.36 3.73-1.36.08 0 .27.02.39.12.1.08.13.19.14.27-.01.06.01.24 0 .38z'
     },
     {
