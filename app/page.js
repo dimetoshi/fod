0b32d1c35a8b84095b6e8ce40d3a1d7c36a9e6f4
dimetoshi <design@clrk.studio>
2025-06-24 07:27:45 +0930

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import ProfileGrid from '@/components/ProfileGrid'
+import DogeNavigation from '@/components/DogeNavigation'
 
 export default function Home() {
   return (
     <main>
+      <DogeNavigation />
       <ProfileGrid />
     </main>
   )
